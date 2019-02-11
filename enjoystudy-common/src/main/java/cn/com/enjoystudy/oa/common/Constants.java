04ec7917a31cb267a17b9b04cbc27a76a2f34613
duyongsheng <duloveping@qq.com>
2019-02-11 15:15:46 +0800

    添加系统常量

---
+++ enjoystudy-common/src/main/java/cn/com/enjoystudy/oa/common/Constants.java
@@ -0,0 +1,39 @@
+package cn.com.enjoystudy.oa.common;
+
+public class Constants {
+    public static final String SECURITY_QUESTION_1 = "我就读的第一所学校的名称？";
+    public static final String SECURITY_QUESTION_2 = "我最喜欢的运动是什么？";
+    public static final String SECURITY_QUESTION_3 = "我最喜欢的运动员是谁？";
+    public static final String SECURITY_QUESTION_4 = "我最喜欢的物品名称？";
+    public static final String SECURITY_QUESTION_5 = "我最喜欢的歌曲？";
+    public static final String SECURITY_QUESTION_6 = "妈妈的生日是？";
+    public static final String SECURITY_QUESTION_7 = "我最喜欢的食物是？";
+    public static final String SECURITY_QUESTION_8 = "我最爱的人的名字？";
+    public static final String SECURITY_QUESTION_9 = "我最爱的电影？";
+    public static final String SECURITY_QUESTION_10 = "爸爸的生日是？";
+    public static final String[] SECURITY_QUESTION_LIST = {SECURITY_QUESTION_1, SECURITY_QUESTION_2, SECURITY_QUESTION_3,
+            SECURITY_QUESTION_4, SECURITY_QUESTION_5, SECURITY_QUESTION_6, SECURITY_QUESTION_7, SECURITY_QUESTION_8,
+            SECURITY_QUESTION_9, SECURITY_QUESTION_10};
+
+    public static final Integer COMMUNICATION_CATEGORY_MYSELF = 1;
+
+    /** 用户类型 - 系统用户 */
+    public static final Integer ACCOUNT_CATEGORY_SYSTEM = 1;
+    /** 用户类型 - 政府单位 */
+    public static final Integer ACCOUNT_CATEGORY_GOVERNMENT = 2;
+    /** 用户类型 - 企业单位 */
+    public static final Integer ACCOUNT_CATEGORY_COMPANY = 3;
+    /** 用户类型 - 学校单位 */
+    public static final Integer ACCOUNT_CATEGORY_SCHOOL = 4;
+    /** 用户类型 - 协会单位 */
+    public static final Integer ACCOUNT_CATEGORY_INSTITUTE = 5;
+    /** 用户类型 - 员工用户 */
+    public static final Integer ACCOUNT_CATEGORY_EMPLOYEE = 6;
+    /** 用户类型 - 老师 */
+    public static final Integer ACCOUNT_CATEGORY_TEACHER = 7;
+    /** 用户类型 - 学生（考生） */
+    public static final Integer ACCOUNT_CATEGORY_STUDENT = 8;
+    /** 用户类型 - 个人 */
+    public static final Integer ACCOUNT_CATEGORY_PERSONAL = 9;
+
+}
